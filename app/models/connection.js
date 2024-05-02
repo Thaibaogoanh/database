b4077457578aca22932092100ec2896f36d2e1ad
phuoclehuy <lehuyphuoc0105@gmail.com>
2024-05-03 01:49:39 +0700

    create new connection to intial database

---
+++ app/models/connection.js
@@ -0,0 +1,28 @@
+const { closeDB, connectDB } = require("./config.js");
+const fs = require("fs");
+
+const sqlScriptPath = "./app/models/db.sql";
+
+async function executeSqlScript() {
+  try {
+    const sqlScript = fs.readFileSync(sqlScriptPath, "utf8");
+    const commands = sqlScript.split(/\bGO\b/);
+
+    const pool = await connectDB();
+    console.log("Connected to MSSQL 🎉");
+    // Remove last empty command if exists
+    if (commands[commands.length - 1].trim() === "") {
+      commands.pop();
+    }
+
+    for (let i = 0; i < commands.length; i++) {
+      await pool.request().query(commands[i]);
+    }
+    console.log("Database setup completed 🚀");
+  } catch (error) {
+    await closeDB();
+    console.log("Connection to MSSQL is closed 🚪");
+  }
+}
+
+executeSqlScript();
