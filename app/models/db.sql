0107b8002be225cb6d5135c35e746d9d184e050c
phuoclehuy <lehuyphuoc0105@gmail.com>
2024-05-02 12:36:08 +0700

    update sql

--- app/models/db.sql
+++ app/models/db.sql
@@ -22,7 +22,7 @@ BEGIN
       [address] NVARCHAR(500) NOT NULL,
       [job_type] NVARCHAR(100) NOT NULL,
       [date_of_work] DATETIME2 NOT NULL DEFAULT GETDATE(),
-      [gender] NVARCHAR(10) NOT NULL,
+      [gender] NVARCHAR(10) NOT NULL CHECK (gender IN ('MALE', 'FEMALE','OTHER')),
       [date_of_birth] DATE NOT NULL,
       [last_name] NVARCHAR(50) NOT NULL,
       [middle_name] NVARCHAR(50) NOT NULL,
@@ -36,25 +36,129 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'employee')
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'employee_phone_number')
 BEGIN
-    CREATE TABLE [employee] (
-      [ssn] INT NOT NULL IDENTITY(1,1),
-      [cccd] NVARCHAR(50) NOT NULL,
-      [address] NVARCHAR(500) NOT NULL,
-      [job_type] NVARCHAR(100) NOT NULL,
-      [date_of_work] DATETIME2 NOT NULL DEFAULT GETDATE(),
-      [gender] NVARCHAR(10) NOT NULL,
+    CREATE TABLE [employee_phone_number] (
+      [ssn] INT NOT NULL,
+      [phone_number] VARCHAR(50) NOT NULL,
+      PRIMARY KEY ([ssn],[phone_number]),
+      CONSTRAINT [fk_emp_phone_number] FOREIGN KEY ([ssn]) REFERENCES [employee] ([ssn]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'full_time_employee')
+BEGIN
+    CREATE TABLE [full_time_employee] (
+      [ssn] INT NOT NULL,
+      [insurance] VARCHAR(50) NOT NULL,
+      [month_salary] DECIMAL(10,2) NOT NULL,
+      PRIMARY KEY ([ssn]),
+      CONSTRAINT [fk_ft_emp] FOREIGN KEY ([ssn]) REFERENCES [employee] ([ssn]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'part_time_employee')
+BEGIN
+    CREATE TABLE [part_time_employee] (
+      [ssn] INT NOT NULL,
+      [hourly_salary] DECIMAL(10,2) NOT NULL,
+      PRIMARY KEY ([ssn]),
+      CONSTRAINT [fk_pt_emp] FOREIGN KEY ([ssn]) REFERENCES [employee] ([ssn]) ON DELETE CASCADE
+    );
+END;
+
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'part_time_employee_works')
+BEGIN
+    CREATE TABLE [part_time_employee_works] (
+      [ssn] INT NOT NULL,
+      [date] DATE NOT NULL,
+      [shift] NVARCHAR(50) NOT NULL CHECK (shift IN ('7:00 AM - 12:00 AM','12:00 AM - 17:00 PM','17:00 PM - 22:00 PM')),
+      PRIMARY KEY ([ssn],[date],[shift]),
+      CONSTRAINT [fk_pt_emp_works] FOREIGN KEY ([ssn]) REFERENCES [employee] ([ssn]) ON DELETE CASCADE
+    );
+END;
+
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'employee_dependent')
+BEGIN
+    CREATE TABLE [employee_dependent] (
+      [ssn] INT NOT NULL,
+      [name] NVARCHAR(50) NOT NULL,
+      [relationship] NVARCHAR(50) NOT NULL,
+      [phone_number] VARCHAR(50) NOT NULL,
+      [address] VARCHAR(500) NOT NULL,
       [date_of_birth] DATE NOT NULL,
-      [last_name] NVARCHAR(50) NOT NULL,
-      [middle_name] NVARCHAR(50) NOT NULL,
-      [first_name] NVARCHAR(50) NOT NULL,
-      [super_ssn] INT DEFAULT NULL,
+      [gender] NVARCHAR(10) NOT NULL,
       [created_at] DATETIME2 NOT NULL DEFAULT GETDATE(),
       [updated_at] DATETIME2 NOT NULL DEFAULT GETDATE(),
-      PRIMARY KEY ([ssn]),
-      UNIQUE ([cccd]),
-      CONSTRAINT [fk_emp_superssn] FOREIGN KEY ([super_ssn]) REFERENCES [employee] ([ssn])
+      PRIMARY KEY ([ssn],[name]),
+      CONSTRAINT [fk_emp_dependent] FOREIGN KEY ([ssn]) REFERENCES [employee] ([ssn]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'supplier_invoice')
+BEGIN
+    CREATE TABLE [supplier_invoice] (
+      [id] INT NOT NULL IDENTITY(1,1),
+      [date] DATE NOT NULL,
+      [time] TIME NOT NULL,
+      PRIMARY KEY ([id])
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'supplier')
+BEGIN
+    CREATE TABLE [supplier] (
+      [supplier_name] NVARCHAR(50) NOT NULL,
+      PRIMARY KEY ([supplier_name])
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'supplier_invoice_item')
+BEGIN
+    CREATE TABLE [supplier_invoice_item] (
+      [product_name] NVARCHAR(50) NOT NULL,
+      [unit] NVARCHAR(50) NOT NULL,
+      [price] DECIMAL(10,2) NOT NULL,
+      PRIMARY KEY ([product_name])
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'supply')
+BEGIN
+    CREATE TABLE [supply] (
+      [quantity] INT NOT NULL,
+      [supplier_name] NVARCHAR(50) NOT NULL,
+      [supplier_invoice_id] INT NOT NULL,
+      [product_name] NVARCHAR(50) NOT NULL,
+      PRIMARY KEY ([supplier_name],[supplier_invoice_id],[product_name]),
+      CONSTRAINT [fk_supply_supplier] FOREIGN KEY ([supplier_name]) REFERENCES [supplier] ([supplier_name]) ON DELETE CASCADE,
+      CONSTRAINT [fk_supply_supplier_invoice] FOREIGN KEY ([supplier_invoice_id]) REFERENCES [supplier_invoice] ([id]) ON DELETE CASCADE,
+      CONSTRAINT [fk_supply_supplier_invoice_item] FOREIGN KEY ([product_name]) REFERENCES [supplier_invoice_item] ([product_name]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'check_supplier_invoice')
+BEGIN
+    CREATE TABLE [check_supplier_invoice] (
+      [ssn] INT NOT NULL,
+      [supplier_invoice_id] INT NOT NULL,
+      PRIMARY KEY ([ssn],[supplier_invoice_id]),
+      CONSTRAINT [fk_emp_check_supplier_invoice] FOREIGN KEY ([ssn]) REFERENCES [employee] ([ssn]) ON DELETE CASCADE,
+      CONSTRAINT [fk_supplier_invoice_check_supplier_invoice] FOREIGN KEY ([supplier_invoice_id]) REFERENCES [supplier_invoice] ([id]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'item_in_store')
+BEGIN
+    CREATE TABLE [item_in_store] (
+      [product_name] NVARCHAR(50) NOT NULL,
+      [unit] NVARCHAR(50) NOT NULL,
+      [remaining_quantity] DECIMAL(10,2) NOT NULL,
+      [supplier_invoice_id] INT NOT NULL,
+      PRIMARY KEY ([product_name]),
+      CONSTRAINT [fk_item_in_store_supplier_invoice] FOREIGN KEY ([supplier_invoice_id]) REFERENCES [supplier_invoice] ([id]) ON DELETE CASCADE
     );
 END;
 
@@ -67,20 +171,26 @@ IF OBJECT_ID('dbo.trigger_DeleteEmployee', 'TR') IS NOT NULL
     DROP TRIGGER dbo.trigger_DeleteEmployee;
 GO
 
-CREATE TRIGGER trigger_DeleteEmployee
+CREATE TRIGGER trg_DeleteEmployee
 ON dbo.employee
-BEFORE DELETE
+INSTEAD OF DELETE
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    UPDATE dbo.employee
-    SET super_ssn = NULL
-    WHERE super_ssn IN (SELECT ssn FROM deleted);
+    UPDATE e
+    SET e.super_ssn = NULL
+    FROM dbo.employee e
+    INNER JOIN deleted d ON e.super_ssn = d.ssn;
+
+    DELETE FROM dbo.employee WHERE ssn IN (SELECT ssn FROM deleted);
 END;
 GO
 
 
+
+-- Insert data
+-- 1. Insert data for employee table
 INSERT INTO [employee] ([cccd], [address], [job_type], [date_of_work], [gender], [date_of_birth], [last_name], [middle_name], [first_name]) 
 VALUES 
 (N'AB123456789', N'123 Đường Mê Linh, Thành phố Hồ Chí Minh', N'Pha chế', '2024-04-30 09:00:00', N'MALE', '1980-05-15', N'Nguyễn', N'Văn', N'An'),
@@ -116,3 +226,47 @@ VALUES
 (N'II234567890', N'901 Đường Phạm Văn Đồng, Thành phố Thủ Dầu Một', N'Phục vụ', '2024-04-19 03:30:00', N'FEMALE', '1993-06-15', N'Mai', N'Thị', N'Hương', 12),
 (N'JJ345678901', N'1001 Đường Nguyễn Thái Học, Thành phố Quy Nhơn', N'Phục vụ', '2024-04-18 03:00:00', N'MALE', '1988-11-10', N'Đoàn', N'Văn', N'Thành', 13),
 (N'KK456789012', N'1101 Đường Lý Thường Kiệt, Thành phố Bắc Ninh', N'Bảo vệ', '2024-04-17 02:30:00', N'FEMALE', '1991-04-05', N'Võ', N'Thị', N'Mỹ', 14);
+
+-- 2. Insert data for employee_phone_number table
+INSERT INTO [employee_phone_number] ([ssn], [phone_number])
+VALUES 
+(1, '0123456789'),
+(2, '0987654321'),
+(3, '0123456789'),
+(4, '0987654321'),
+(5, '0123456789'),
+(6, '0987654321'),
+(7, '0123456789'),
+(8, '0987654321'),
+(9, '0123456789'),
+(10, '0987654321'),
+(11, '0123456789'),
+(12, '0987654321'),
+(13, '0123456789'),
+(14, '0987654321'),
+(15, '0123456789'),
+(16, '0987654321'),
+(17, '0123456789'),
+(18, '0987654321'),
+(19, '0123456789'),
+(20, '0987654321'),
+(21, '0123456789'),
+(22, '0987654321'),
+(23, '0123456789'),
+(24, '0987654321'),
+(25, '0123456789'),
+(26, '0987654321'),
+(27, '0123456789'),
+(28, '0987654321'),
+(29, '0123456789'),
+(30, '0987654321'),
+(1, '0999999999'),
+(2, '0888888888'),
+(3, '0777777777'),
+(4, '0123123444'),
+(5, '7877867687'),
+(6, '2344324234'),
+(7, '5454123534'),
+(8, '4321231231'),
+(9, '0738274823'),
+(10, '0782374861');
