8c5ce4925b655d2c79ee8f4cf3b7c2df5c87658b
phuoclehuy <lehuyphuoc0105@gmail.com>
2024-05-03 15:37:44 +0700

    change index file to employee

---
+++ app/routes/employee.js
@@ -0,0 +1,25 @@
+module.exports = (app) => {
+  const employees = require("../controllers/employee.js");
+  const express = require("express");
+  const router = express.Router();
+
+  // Create a new employee
+  router.post("/", employees.create);
+
+  // Fitler employees with job type
+  router.get("/", employees.findByFilter);
+
+  // Get all employees
+  router.get("/", employees.findAll);
+
+  // Get an employee with id
+  router.get("/:id", employees.findOne);
+
+  // Update an employee with id
+  router.put("/:id", employees.update);
+
+  // Delete an employee with id
+  router.delete("/:id", employees.delete);
+
+  app.use("/api/v1/employees", router);
+};
