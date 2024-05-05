7d90aa24675eb434d43369d8473fe7974f20d09a
phuoclehuy <lehuyphuoc0105@gmail.com>
2024-05-06 02:10:13 +0700

    fix route employee

--- app/routes/employee.js
+++ app/routes/employee.js
@@ -1,26 +1,26 @@
 module.exports = (app) => {
-  const employees = require("../controllers/employee.js");
+  const employee = require("../controllers/employee.js");
   const { uploadEmployee, uploadProduct } = require("../models/config.js");
   const express = require("express");
   const router = express.Router();
-  
+
   // Create a new employee
-  router.post("/", uploadEmployee.single("image"), employees.create);
+  router.post("/", uploadEmployee.single("image"), employee.create);
 
   // Fitler employees with job type
-  router.get("/", employees.findByFilter);
+  router.get("/", employee.findByFilter);
 
   // Get employees image
-  router.get("/images/:imageName", employees.getImage);
+  router.get("/images/:imageName", employee.getImage);
 
   // Get an employee with id
-  router.get("/:id", employees.findById);
+  router.get("/:id", employee.findById);
 
   // Update an employee with id
-  router.put("/:id", uploadEmployee.single("image"), employees.update);
+  router.put("/:id", uploadEmployee.single("image"), employee.update);
 
   // Delete employees with ids
-  router.delete("/", employees.delete);
+  router.delete("/", employee.delete);
 
   app.use("/api/v1/employees", router);
 };
