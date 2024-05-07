e814e6dc33512e4f89679c0e0dfe0c6343580ad8
ThanhKhanh <nguyenthanhkhanh2304@gmail.com>
2024-05-08 01:34:24 +0700

    update fe for product

---
+++ src/pages/Product/data.js
@@ -0,0 +1,423 @@
+const pd_data = [
+    {
+    "beverage_name": "Cà phê đen",
+    "image_url": "http://localhost:5000/api/v1/products/images/2.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 20000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 1
+    },
+    {
+    "size": "Vừa",
+    "price": 15000,
+    "sold_quantity": 2
+    }
+    ]
+    },
+    {
+    "beverage_name": "Cà phê sữa",
+    "image_url": "http://localhost:5000/api/v1/products/images/1.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 30000,
+    "sold_quantity": 11
+    },
+    {
+    "size": "Nhỏ",
+    "price": 20000,
+    "sold_quantity": 22
+    },
+    {
+    "size": "Vừa",
+    "price": 25000,
+    "sold_quantity": 33
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước cam",
+    "image_url": "http://localhost:5000/api/v1/products/images/3.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 15000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 5
+    },
+    {
+    "size": "Vừa",
+    "price": 12000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước chanh",
+    "image_url": "http://localhost:5000/api/v1/products/images/5.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 15000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 12000,
+    "sold_quantity": 5
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước dừa",
+    "image_url": "http://localhost:5000/api/v1/products/images/4.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 20000,
+    "sold_quantity": 6
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 15000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước ép dưa hấu",
+    "image_url": "http://localhost:5000/api/v1/products/images/11.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 35000,
+    "sold_quantity": 10
+    },
+    {
+    "size": "Nhỏ",
+    "price": 25000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 30000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước ép lê",
+    "image_url": "http://localhost:5000/api/v1/products/images/14.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 25000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 15000,
+    "sold_quantity": 1
+    },
+    {
+    "size": "Vừa",
+    "price": 20000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước ép lựu",
+    "image_url": "http://localhost:5000/api/v1/products/images/7.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 35000,
+    "sold_quantity": 3
+    },
+    {
+    "size": "Nhỏ",
+    "price": 25000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 30000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước ép mơ",
+    "image_url": "http://localhost:5000/api/v1/products/images/15.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 30000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 20000,
+    "sold_quantity": 2
+    },
+    {
+    "size": "Vừa",
+    "price": 25000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước ép táo",
+    "image_url": "http://localhost:5000/api/v1/products/images/13.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 20000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 15000,
+    "sold_quantity": 3
+    }
+    ]
+    },
+    {
+    "beverage_name": "Nước ép thập cẩm",
+    "image_url": "http://localhost:5000/api/v1/products/images/20.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 35000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 25000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 30000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố bơ",
+    "image_url": "http://localhost:5000/api/v1/products/images/8.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 50000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 40000,
+    "sold_quantity": 5
+    },
+    {
+    "size": "Vừa",
+    "price": 45000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố chuối",
+    "image_url": "http://localhost:5000/api/v1/products/images/9.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 20000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 3
+    },
+    {
+    "size": "Vừa",
+    "price": 15000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố dâu",
+    "image_url": "http://localhost:5000/api/v1/products/images/6.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 25000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 15000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 20000,
+    "sold_quantity": 4
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố dừa xiêm",
+    "image_url": "http://localhost:5000/api/v1/products/images/17.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 30000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 20000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 25000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố mận",
+    "image_url": "http://localhost:5000/api/v1/products/images/16.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 20000,
+    "sold_quantity": 2
+    },
+    {
+    "size": "Nhỏ",
+    "price": 10000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 15000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố nho",
+    "image_url": "http://localhost:5000/api/v1/products/images/12.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 50000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 40000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 45000,
+    "sold_quantity": 2
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố nho khô",
+    "image_url": "http://localhost:5000/api/v1/products/images/18.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 45000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 35000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 40000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố thập cẩm",
+    "image_url": "http://localhost:5000/api/v1/products/images/19.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 50000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Nhỏ",
+    "price": 40000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 45000,
+    "sold_quantity": 0
+    }
+    ]
+    },
+    {
+    "beverage_name": "Sinh tố xoài",
+    "image_url": "http://localhost:5000/api/v1/products/images/10.jpeg",
+    "sizes": [
+    {
+    "size": "Lớn",
+    "price": 30000,
+    "sold_quantity": 7
+    },
+    {
+    "size": "Nhỏ",
+    "price": 20000,
+    "sold_quantity": 0
+    },
+    {
+    "size": "Vừa",
+    "price": 25000,
+    "sold_quantity": 0
+    }
+    ]
+    }
+]
+export default pd_data;
